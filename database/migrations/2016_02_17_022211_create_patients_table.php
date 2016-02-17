4881dc488821c204c61dd7f47d6a0b8d1858a9cc
Fredrick Abayie <fredrick.abayie@gmail.com>
2016-02-17 02:25:08 +0000

    CreatePatientsTable
    Created a migration for creating a patients table in the database.

---
+++ database/migrations/2016_02_17_022211_create_patients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Create patients table in the database
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Drop patients table in the table
+        Schema::drop('patients');
+    }
+}
